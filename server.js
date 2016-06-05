53ad0dc857ee7a8aee130f06e683761f5a313c5b
TedtheDev <tylerjedmunds@gmail.com>
2016-06-05 15:29:57 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+exports.startNodeServer = function()
+{
+  var fs = require("fs");
+  fs.readFile('./helloWorld.html', function (err, html)
+  {
+    if(err)
+    {
+      throw err;
+    }
+    console.log("Initiating Server");
+    var http = require("http");
+
+    console.log("Starting Server");
+    http.createServer(function(request, response){
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(html);
+      response.end();
+    }).listen(8888);
+
+    console.log("Server Running");
+    }
+  );
+}

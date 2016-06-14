4acd04c798141577911d44176d738920dd8d7e06
TedtheDev <tylerjedmunds@gmail.com>
2016-06-13 19:36:30 -0500

    adding

---
+++ expressDB.js
@@ -0,0 +1,15 @@
+var MongoClient = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/test', function( err, db) {
+  if(err)
+  {
+    throw err;
+  }
+  db.collection('restaurants').find().toArray(function(err, result) {
+    if(err)
+    {
+      throw err;
+    }
+    console.log(result);
+  });
+});
--- serverExpress.js
+++ serverExpress.js
@@ -1,13 +1,21 @@
+//decalre varaibles
 var express = require('express');
 var app = express();
 
+//set jade as template engine
+app.set('view engine', 'jade');
+
+//set directories for html and css
 app.use('/html', express.static('helloWorld'));
 app.use('/css', express.static('css'));
 
-app.get('/', function( req, res) {
-  res.send('Hello World!@!@!');
+//get landing page
+app.get('/html/', function( req, res) {
+  res.render('helloWorldJade');
+  //res.send('Hello World!@!@!');
 });
 
+//run server and listen
 app.listen(3000, function() {
   console.log('Listening on 3000');
 });

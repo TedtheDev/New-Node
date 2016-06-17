a83687f6b92d85b31e9d0bd71e9dbf80ecce8b0d
TedtheDev <tylerjedmunds@gmail.com>
2016-06-16 23:13:49 -0500

    IT WORKS

--- angularjs/myControllers.js
+++ angularjs/myControllers.js
@@ -2,6 +2,17 @@ app.controller('myController', function($scope) {
   $scope.theMessageForHelloWorld = "Hello World from AngularJS";
 });
 
-app.controller('getRecords', function($scope) {
-  $scope.restaurants = ["Vella", "Sampson Burgers", "Tacos"];
+app.controller('getRestaurants', function($scope, $http) {
+  $scope.getRestaurants = function() {
+    $http.get('/api/restaurants')
+      .success(function(data) {
+        //var theRestaurants = JSON.parse(data)
+        $scope.restaurants = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+  };
+
 });
--- newServer.js
+++ newServer.js
@@ -2,9 +2,25 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
-var database = require('./databaseStuff')
+//var database = require('./databaseStuff')
 
 app.use(morgan('dev'));
+
+//set jade as template engine
+app.set('views', './helloWorld')
+app.set('view engine', 'jade');
+
+//set directories for html and css
+app.use('/html', express.static('helloWorld'));
+app.use('/css', express.static('css'));
+app.use('/angularjs', express.static('angularjs'));
+app.use('/node_modules', express.static('node_modules'));
+
+app.get('/html/', function( req, res) {
+  res.render('helloWorldJade');
+  //res.sendFile(__dirname + '/helloWorld/angularWithPureHTML.html');
+  //res.send('Hello World!@!@!');
+});
 
 app.listen(3000, function() {
   console.log('Listening on 3000');
@@ -15,27 +31,19 @@ app.listen(3000, function() {
 app.get('/', function(req, res) {
   res.send('It is working');
 });
+
 app.get('/api/restaurants', function(req, res) {
   var MongoClient = require("mongodb").MongoClient;
   var assert = require("assert");
   var ObjectId = require("mongodb").ObjectId;
   var url = "mongodb://localhost:27017/test";
   var docArray = new Array();
+
   MongoClient.connect(url, function(err, db) {
       assert.equal(null, err);
-      var cursor = db.collection("restaurants").find( { "name" : "Juni" } );
-      cursor.each(function(err, doc) {
-        assert.equal(err, null);
-        if (doc != null)
-        {
-          console.dir(doc);
-          docArray.push(doc);
-        }
-        else
-        {
-          db.close();
-        }
+      db.collection("restaurants").find().toArray(function(err, docs) {
+        res.json(docs);
+        db.close();
       });
-  });
-  res.json(docArray[0]);
+    });
 });

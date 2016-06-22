06993685faf310e9bcb7f0963cbfdc79ae05f00e
TedtheDev <tylerjedmunds@gmail.com>
2016-06-22 18:27:57 -0500

    Stylus working

--- newServer.js
+++ newServer.js
@@ -4,62 +4,52 @@ var app = express();
 var morgan = require('morgan');
 var stylus = require('stylus');
 var nib = require('nib');
+var path = require('path');
 
 
 // This must be BEFORE other app.use
+//this function is to compile stylus file into css
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
     .use(nib())
-};
+    .set('compress', true)
+    .import('nib');
+}
 
-function compile2(str, path) {
-   return stylus(str)
-     .set('filename', path)
-     .set('compress', true)
-     .use(nib())
-     .import('nib');
- }
-app.use(stylus.middleware(
-  { src: '/stylus',
-    dest: '/css',
-    compile: compile2
-  }
-));
+//compile stylus
+app.use(stylus.middleware({
+  src: __dirname + '/resources',
+  dest: __dirname + '/public',
+  debug: true,
+  force: true,
+  compile: compile
+}));
+app.use(express.static('public'));
 
-
-
+//set dev env
 app.use(morgan('dev'));
 
 //set jade as template engine
-app.set('views', './helloWorld');
+app.set('views', 'views');
 app.set('view engine', 'jade');
 
 //set directories for html and css
-app.use('/html', express.static('helloWorld'));
-app.use('/css', express.static('css'));
+app.use('/css', express.static('public/stylesheets'));
 app.use('/angularjs', express.static('angularjs'));
 app.use('/node_modules', express.static('node_modules'));
-app.use('/stylus', express.static('stylus'));
 
-
-
-app.get('/html/', function( req, res) {
+// landing page
+app.get('/', function( req, res) {
   res.render('helloWorldJade');
-  //res.sendFile(__dirname + '/helloWorld/angularWithPureHTML.html');
-  //res.send('Hello World!@!@!');
 });
 
+//listening
 app.listen(3000, function() {
   console.log('Listening on 3000');
 });
 
-
-
-app.get('/', function(req, res) {
-  res.send('It is working');
-});
-
+//api to get restaurants
 app.get('/api/restaurants', function(req, res) {
   var MongoClient = require("mongodb").MongoClient;
   var assert = require("assert");
@@ -70,7 +60,10 @@ app.get('/api/restaurants', function(req, res) {
   MongoClient.connect(url, function(err, db) {
       assert.equal(null, err);
       db.collection("restaurants").find().limit(5).toArray(function(err, docs) {
-        res.json(docs);
+        if(err)
+          throw err;
+        else
+          res.json(docs);
         db.close();
       });
     });

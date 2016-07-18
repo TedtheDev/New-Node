c8f55104e17688638c8a094ce92604d5c63f04b7
TedtheDev <tylerjedmunds@gmail.com>
2016-07-18 00:20:13 -0500

    added content to serve content

--- angularjs/app.js
+++ angularjs/app.js
@@ -1,9 +1,9 @@
-var app = angular.module("myApp", ['ngRoute']);
+var app = angular.module("myApp", ['ngRoute', 'ngSanitize']);
 
-  app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-      when('/views/about', {
-        templateUrl: 'views/about',
-        controller: 'getContentViews' }).
-      otherwise({redirectTo: '/'});
-  }]);
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/views/about', {
+      templateUrl: 'views/about',
+      controller: 'getContentViews' }).
+    otherwise({redirectTo: '/'});
+}]);

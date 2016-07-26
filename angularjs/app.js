3466b2838a071f24dec85e02c15d218b7ec010db
TedtheDev <tylerjedmunds@gmail.com>
2016-07-25 20:51:43 -0500

    fixed portfolio, was a typo

--- angularjs/app.js
+++ angularjs/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ['ngRoute']);
+var app = angular.module("myApp", ['ngRoute', 'ngSanitize']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.

713f4ca5899dde1151691c9e4c1c6a94626f7181
TedtheDev <tylerjedmunds@gmail.com>
2016-08-01 20:32:33 -0500

    working on portfolio smooth scroll, but will add later

--- angularjs/app.js
+++ angularjs/app.js
@@ -1,9 +1,1 @@
-var app = angular.module("myApp", ['ngRoute', 'ngSanitize']);
-
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-    when('/views/about', {
-      templateUrl: 'views/about',
-      controller: 'getContentViews' }).
-    otherwise({redirectTo: '/'});
-}]);
+var app = angular.module("myApp", []);

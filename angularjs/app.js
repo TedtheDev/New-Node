b5d37c445346f7ed281c38bcac9e5f02f39ffa51
TedtheDev <tylerjedmunds@gmail.com>
2016-08-18 21:46:02 -0500

    added ui-router and states

--- angularjs/app.js
+++ angularjs/app.js
@@ -1,1 +1,42 @@
-var app = angular.module("myApp", ['ngAnimate']);
+var app = angular.module("myApp", ['ngAnimate','ui.router']);
+
+
+
+
+app.config(function($stateProvider) {
+  $stateProvider
+    .state('index',  {
+      url: "",
+      views: {
+        "index": {
+          templateUrl: "/views/indexContent"
+        }
+      }
+    })
+    .state('home', {
+      parent: "index",
+      url: '/home',
+      templateUrl: "/views/home"
+    })
+    .state('about', {
+      parent: "index",
+      url: '/about',
+      templateUrl: "/views/about"
+    })
+    .state('portfolio', {
+      parent: "index",
+      url: '/portfolio',
+      templateUrl: "/views/portfolio"
+    })
+    .state('github', {
+      parent: "index",
+      url: '/github',
+      templateUrl: "/views/github"
+    })
+    .state('contact', {
+      parent: "index",
+      url: '/contact',
+      templateUrl: "/views/contact"
+    })
+
+});

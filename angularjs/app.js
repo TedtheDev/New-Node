220c6887f280e5a48766bbe29196f55c26839627
TedtheDev <tylerjedmunds@gmail.com>
2016-09-05 23:09:36 -0500

    added urls for projects, problem with payroll system working

--- angularjs/app.js
+++ angularjs/app.js
@@ -16,27 +16,109 @@ app.config(function($stateProvider) {
     .state('home', {
       parent: "index",
       url: '/home',
-      templateUrl: "/views/home"
+      views: {
+        "content": {
+          templateUrl: "/views/home"
+        }
+      }
     })
     .state('about', {
       parent: "index",
       url: '/about',
-      templateUrl: "/views/about"
+      views: {
+        "content": {
+          templateUrl: "/views/about"
+        }
+      }
     })
     .state('portfolio', {
       parent: "index",
       url: '/portfolio',
-      templateUrl: "/views/portfolio"
+      views: {
+        "content": {
+          templateUrl: "/views/portfolio"
+        }
+      }
     })
     .state('github', {
       parent: "index",
       url: '/github',
-      templateUrl: "/views/github"
+      views: {
+        "content": {
+          templateUrl: "/views/github"
+        }
+      }
     })
     .state('contact', {
       parent: "index",
       url: '/contact',
-      templateUrl: "/views/contact"
+      views: {
+        "content": {
+          templateUrl: "/views/contact"
+        }
+      }
     })
-
+    .state('firstWebsite', {
+      parent: "portfolio",
+      url: '/firstwebsite',
+      views: {
+        "loadProject": {
+          templateUrl: "/projects/firstWebsite"
+        }
+      }
+    })
+    .state('wordpress', {
+      parent: "portfolio",
+      url: '/webcontent',
+      views: {
+        "loadProject": {
+          templateUrl: "/projects/wordpress"
+        }
+      }
+    })
+    .state('esss', {
+      parent: "portfolio",
+      url: '/esss',
+      views: {
+        "loadProject": {
+          templateUrl: "/projects/esssProject"
+        }
+      }
+    })
+    .state('payrollsystem', {
+      parent: "portfolio",
+      url: '/payrollsystem',
+      views: {
+        "loadProject": {
+          templateUrl: "/projects/payrollSystem"
+        }
+      }
+    })
+    .state('lampProject', {
+      parent: "portfolio",
+      url: '/lampproject',
+      views: {
+        "loadProject": {
+          templateUrl: "/projects/lampProject"
+        }
+      }
+    })
+    .state('lanProject', {
+      parent: "portfolio",
+      url: '/lanproject',
+      views: {
+        "loadProject": {
+          templateUrl: "/projects/lanProject"
+        }
+      }
+    })
+    .state('nodeWebsite', {
+      parent: "portfolio",
+      url: '/nodewebsite',
+      views: {
+        "loadProject": {
+          templateUrl: "/projects/nodeWebsite"
+        }
+      }
+    })
 });
